c65c5287a5d07ae428310383079ae4503485b949
Jeff Menter <jmenter@gmail.com>
2015-05-23 08:40:32 -0500

    added version.h

---
+++ src/openalpr/version.h
@@ -0,0 +1,7 @@
+#ifndef openalpr_xcode_version_h
+#define openalpr_xcode_version_h
+#endif
+
+#define OPENALPR_MAJOR_VERSION "1"
+#define OPENALPR_MINOR_VERSION "2"
+#define OPENALPR_PATCH_VERSION "0"
